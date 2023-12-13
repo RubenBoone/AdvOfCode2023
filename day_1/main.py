7294d00b37f132a210b3f56f8f4d9e4711f95038
Ruben Boone <ruben.boone01@gmail.com>
2023-12-13 19:13:51 +0100

    day 1 part 2

--- day_1/main.py
+++ day_1/main.py
@@ -1,29 +1,37 @@
 def part1():
     file = open("./puzzle_input.txt", "r")
+    numbers_spelled_out = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
     answer = 0
 
+    line_nr = 1
+
     for line in file.readlines():
-        
+        possible_word = ""
         first_number = 0
         last_number = 0
+        number = 0
 
         for char in line:
             try:
-                if first_number == 0:
-                    first_number = int(char)
-                else:
-                    last_number = int(char)
+                number = int(char)
             except ValueError:
-                pass
+                possible_word += char
+                for word in numbers_spelled_out:
+                    if word in possible_word:
+                        number = numbers_spelled_out.index(word) + 1
+                        possible_word = possible_word[-1:]
+                        break
 
-        if last_number == 0:
-            last_number = first_number
+            if first_number == 0:
+                first_number = number
+            else:
+                last_number = number
         
         answer += int(str(first_number) + str(last_number))
+        line_nr += 1
 
     print(answer)
-
 
 if __name__ == "__main__":
     part1()
