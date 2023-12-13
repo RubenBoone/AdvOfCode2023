c0e3a91f845b2700215bd5585ab8290bcc3f4b3d
Ruben Boone <ruben.boone01@gmail.com>
2023-12-13 20:04:35 +0100

    day 2 part 2

--- day_2/main.py
+++ day_2/main.py
@@ -1,30 +1,38 @@
 def main():
-    max_cubes = {"red": 12, "green": 13, "blue": 14}
+    max_cubes = {"red": 0, "green": 0, "blue": 0}
+    multiplication_per_grab = []
 
     file = open("./day_2/puzzle_input.txt", "r")
-
-    answer = 0
 
     for line in file.readlines():
         id, grabs = line.split(": ")
         id = int(id.split(" ")[1])
         grabs = grabs.rstrip("\n").split("; ")
 
-        possible = True
         for grab in grabs:
             piece = grab.split(", ")
             for cube in piece:
                 amount, color = cube.split(" ")
                 if int(amount) > max_cubes[color]:
-                    possible = False
-                    break
+                    max_cubes[color] = int(amount)
         
-        if possible:
-            answer += id
+        
+        multiplication = 1
+        for color in max_cubes.values():
+            multiplication *= color
+
+        multiplication_per_grab.append(multiplication)
+
+        max_cubes = {"red": 0, "green": 0, "blue": 0}
 
     file.close()
 
+    answer = 0
+    for number in multiplication_per_grab:
+        answer += number
+
     print(answer)
+
 
 if __name__ == "__main__":
     main()
