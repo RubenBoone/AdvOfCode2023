c56b1f92b5fb96aced157c1a54e07bdf074a3724
Ruben Boone <ruben.boone01@gmail.com>
2023-12-21 14:29:17 +0100

    day 5 part 1 OOM but better

--- day_5/main.py
+++ day_5/main.py
@@ -1,99 +1,51 @@
-import time
-
-
 def main():
     file = open("puzzle_input.txt", "r")
 
-    # Get wanted seeds for the end
-    wanted_seeds = file.readline().rstrip("\n").split(": ")[1].split(" ")
-    wanted_seeds = array_values_to_int(wanted_seeds)
-    max_seed = 0
+    wanted_seed = get_wanted_seeds(file.readline())
+    file.readline()
 
-    # Skip empty line
-    file.readline().rstrip("\n")
-
-    # Get map blocks
-    map_blocks = [[]]
-    for line in file.readlines():
-
-        if line != "\n":
-            map_blocks[-1].append(line.rstrip("\n"))
-        else:
-            map_blocks.append([])
-
-    for block in map_blocks:
-        for line in block:
-
-            if len(line.split(" ")) < 3:
-                continue
-
-            for number in line.split(" "):
-                if int(number) > max_seed:
-                    max_seed = int(number)
-
-    dics = []
-    for block in map_blocks:
-        dics.append({})
-        for line in block:
-
-            if len(line.split(" ")) < 3:
-                continue
-
-            for number in line.split(" "):
-                if int(number) > max_seed:
-                    max_seed = int(number)
-
-
-        for line in block:
-            print("running", time.time())
-            # if line is not splittable in 2 parts, skip it
-            if len(line.split(" ")) < 3:
-                continue
-
-            work_line = [int(x) for x in line.split(" ")]
-            destination_range = work_line[0]
-            source_range = work_line[1]
-            range_length = work_line[2]
-
-            mapping(dics[-1], destination_range, source_range, range_length) 
-
-        need_adding = []
-        for i in range(0, max_seed + 1):
-            if i not in dics[-1].keys():
-                need_adding.append(i)
-
-        for seed in need_adding:
-            dics[-1][seed] = seed
-        
+    data = file.readlines()
     file.close()
 
-    lowest_location = max_seed
-    for seed in wanted_seeds:
-        next_lookup = seed
-        last_lookup = -1
-        for dic in dics:
-            next_lookup = dic[next_lookup]
-            last_lookup = next_lookup
+    blocks = [[]]
+    for line in data:
+        if len(line.split()) < 2:
+            blocks.append([])
+            continue
+
+        try:
+            work_line = [int(x) for x in line.rstrip("\n").split()]
+            blocks[-1].append(work_line)
+        except ValueError:
+            pass
         
-        if last_lookup < lowest_location:
-            lowest_location = last_lookup
+    possible_answer = []
+    for seed in wanted_seed:
+        lookup_number = seed
+        for block in blocks:
+            dic = {}
+            for line in block:
+                destination_range = line[0]
+                source_range = line[1]
+                range_length = line[2]
 
-    print("Lowest location:", lowest_location)
-        
-def array_values_to_int(array):
-    new_array = []
+                for i in range(range_length):
+                    dic[source_range + i] = destination_range + i
 
-    for item in array:
-        new_array.append(int(item))
+            if lookup_number in dic.keys():
+                lookup_number = dic[lookup_number]
+        possible_answer.append(lookup_number)
 
-    return new_array
+    answer = possible_answer[0]
+    for number in possible_answer:
+        if number < answer:
+            answer = number
 
-def mapping(dic, dest, source, r_length):
-    for length in range(r_length):
-        dic[source + length] = dest + length
+    print(answer)
 
-if __name__ == "__main__":
-    start_time = time.time()
+def get_wanted_seeds(line):
+    line = line.split(": ")[1]
+    return [int(x) for x in line.split(" ")]
+
+if __name__ == '__main__':
     main()
-    end_time = time.time()
-    print("Execution time:", end_time - start_time, "seconds")
--- day_5/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-        for item in sorted(dics[-1].keys()):
-            print(f"Seed {item} => Soil {dics[-1][item]}")
