851e72d971ca6b0208be401abd287458f41e3520
Ruben Boone <ruben.boone01@gmail.com>
2023-12-21 15:29:10 +0100

    day 5 part 1

--- day_5/main.py
+++ day_5/main.py
@@ -1,51 +1,35 @@
 def main():
-    file = open("puzzle_input.txt", "r")
+    with open('puzzle_input.txt') as file:
+        lines = file.read().splitlines()
 
-    wanted_seed = get_wanted_seeds(file.readline())
-    file.readline()
+        wanted_seeds = [int(x) for x in lines[0].split(': ')[1].split(' ')]
 
-    data = file.readlines()
+        print(f"Wanted seeds: {wanted_seeds}")
+
+        possible_answers = []
+        for seed in wanted_seeds:
+            next_value = seed
+            skip_block = False
+            for i in range(2, len(lines)):
+
+                if skip_block:
+                    if lines[i] == '':
+                        skip_block = False
+                    continue
+
+                if len(lines[i].split(' ')) <= 2:
+                    continue
+                else:
+                    work_line = [int(x) for x in lines[i].split(' ')]
+                    if next_value >= work_line[1] and next_value <= work_line[1] + work_line[2]:
+                        next_value += (work_line[0] - work_line [1])
+                        skip_block = True
+
+            possible_answers.append(next_value)
     file.close()
 
-    blocks = [[]]
-    for line in data:
-        if len(line.split()) < 2:
-            blocks.append([])
-            continue
-
-        try:
-            work_line = [int(x) for x in line.rstrip("\n").split()]
-            blocks[-1].append(work_line)
-        except ValueError:
-            pass
-        
-    possible_answer = []
-    for seed in wanted_seed:
-        lookup_number = seed
-        for block in blocks:
-            dic = {}
-            for line in block:
-                destination_range = line[0]
-                source_range = line[1]
-                range_length = line[2]
-
-                for i in range(range_length):
-                    dic[source_range + i] = destination_range + i
-
-            if lookup_number in dic.keys():
-                lookup_number = dic[lookup_number]
-        possible_answer.append(lookup_number)
-
-    answer = possible_answer[0]
-    for number in possible_answer:
-        if number < answer:
-            answer = number
-
-    print(answer)
-
-def get_wanted_seeds(line):
-    line = line.split(": ")[1]
-    return [int(x) for x in line.split(" ")]
+    print("Possible answers:", possible_answers)
+    print("Answer:", min(possible_answers))
 
 if __name__ == '__main__':
     main()
